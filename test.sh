67ab371cd8e51ce19c928004b61d64b68d68dfe4
goncalo-leal <goncalolealsilva@ua.pt>
2022-06-18 18:40:23 +0100

    testing done, production not done

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+printf "[*] MOVING FILES TO TESTING ENVIRONMENT...\n"
+
+filename='./tqs_webapp/Dockerfile'
+DIR='test_env'
+
+if [ -d "$DIR" ]; then
+	rm -rf test_env
+    printf "[*] DONE...\n"
+fi
+
+mkdir test_env
+mv -v ./* ./test_env/
+
+cd test_env
+
+if [ -f $filename ]; then
+   rm ./tqs_webapp/Dockerfile
+    printf "[*] DELETED DOCKERFILE...\n"
+fi
+
+cat ./testing/Dockerfile >> ./tqs_webapp/Dockerfile
+
+./dev.sh -a
