786a3da2450665d999c5e94ae9930aee0320c836
goncalo-leal <goncalolealsilva@ua.pt>
2022-06-19 13:29:17 +0100

    cd pipeline

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+printf "[*] MOVING FILES TO DEPLOYMENT ENVIRONMENT...\n"
+
+filename='./tqs_webapp/Dockerfile'
+DIR='prod_env'
+
+if [ -d "$DIR" ]; then
+	rm -rf prod_env
+    printf "[*] DONE...\n"
+fi
+
+mkdir prod_env
+mv -v ./* ./prod_env/
+
+cd prod_env
+
+if [ -f $filename ]; then
+    rm ./tqs_webapp/Dockerfile
+    printf "[*] DELETED DOCKERFILE...\n"
+fi
+
+cat ./deployment/Dockerfile >> ./tqs_webapp/Dockerfile
+
+./prod.sh -a
